655ebff2a7d1ccb4f58ab98a2652c4bd3aef3bcf
Ayoub El Harraoui <ayoub.elharraoui@gmail.com>
2023-11-21 08:44:08 -0800

    returning to these tasks to improve my problem solving skills

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possibl different combinations of 3 digits
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 49; j <= 56; j++)
+		{
+			for (k = 50; k <= 55; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
